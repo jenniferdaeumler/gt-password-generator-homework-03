84362b3e817d80e6450725e8f43ce1d680107843
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 16:19:56 -0500

    var for characterType empty array, var for all character types with confirm and log

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ function generatePassword() {
   console.log("generatePassword initiated");
   var passwordLength = prompt ("Choose your password length by entering a number between 8 and 128.");
   console.log(passwordLength);
+
+  var characterTypes = [];
+
+  var lowerCase = confirm("Would you like to include lowercase characters?");
+  console.log(lowerCase);
+  var upperCase = confirm("Would you like to include uppercase characters?");
+  console.log(upperCase);
+  var numbers = confirm("Would you like to include numbers?");
+  console.log(numbers);
+  var special = confirm("Would you like to include special characters?");
+  console.log(special);
 }
 
 // Write password to the #password input
