daa329897258a89f372581446ffff5feca2ab113
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 09:23:11 -0500

    button clickable with length pop up

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordLength = prompt(
+    "Please enter desired password length between 8 and 128 characters"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a password between 8 and 128 characters");
+    writePassword();
 
-  passwordText.value = password;
+  }}
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// console.log("You clicked my button!");
+// var password = generatePassword();
+// var passwordText = document.querySelector("#password");
+
+// passwordText.value = password;
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword); 
