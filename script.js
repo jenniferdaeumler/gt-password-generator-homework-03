d6f5a338fcec611ab3422f2a3c59b3430bd30fbd
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 13:29:30 -0500

    console log passwordLength

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+ //Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword); 
+
 // Write password to the #password input
 function writePassword() {
+  console.log("You clicked the button");
   var passwordLength = prompt(
     "Please enter password length between 8 and 128 characters."
   );
+  console.log(passwordLength);
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters.");
+  
   } else {
     var upperCase = confirm("Do you want uppercase letter?"),
       lowerCase = confirm("Do you want lowercase letter?"),
       numeric = confirm("Do you want numeric characters?"),
       special = confirm("Do you want special characters?");
   }
+
+
+
   var upperCaseOptions = [
     "A",
     "B",
@@ -93,22 +103,21 @@ function writePassword() {
   }
   console.log(userChoice);
 
-var userPassword = ("k");
+var userPassword = ("");
 
   for (i = 0; i < passwordLength; i++) {
-    userPassword += passwordLength.charAt()
+    userPassword = passwordLength.charAt()
      Math.floor(Math.random() * userChoice.length)
     ;} }
 
- //why does getting rid of 104 make button go away, and pop up automatically, but with it, its undefined?
+ //why does getting rid of 104 make button go away, and pop up automatically, but with it, its undefined? 
+ //also, if I move it anywhere else, the buttons/box disappears. Why would console.log do that?
     console.log(userPassword);
 
 
- var password = writePassword();
+var password = writePassword();
 var passwordText = document.querySelector("#password");
 
 passwordText.value = password;
 
- //Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
 
