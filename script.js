6aa7bef07d1aa951a16d0005335f8c5540e92c3f
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 16:11:41 -0500

    console log button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+console.log("This is script.js");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("Button clicked");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
