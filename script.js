1ed66a8b84e9e327026227974d8cd758309b2b30
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 09:36:59 -0500

    type of characters confirms

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var passwordLength = prompt(
-    "Please enter desired password length between 8 and 128 characters"
+    "Please enter password length between 8 and 128 characters."
   );
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a password between 8 and 128 characters");
-    writePassword();
+    alert("Your password must be between 8 and 128 characters.");}
+  else
+  {var upperCase = confirm("Do you want uppercase letter?"),
+  lowers= confirm("Do you want lowercase letter?"),
+  numeric = confirm("Do you want numeric characters?"),
+  special = confirm("Do you want special characters?");
 
   }}
 
