2a8ce7f08a88c1d4e9dc195cc8a61d73cb674c8e
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-05 16:26:05 -0500

    if statments for characterTypes, push to characterType array, console the final characterType array.

--- script.js
+++ script.js
@@ -6,19 +6,36 @@ var generateBtn = document.querySelector("#generate");
 //Function for generatePassword to be used in writePassword function
 function generatePassword() {
   console.log("generatePassword initiated");
-  var passwordLength = prompt ("Choose your password length by entering a number between 8 and 128.");
+  var passwordLength = prompt("Choose your password length by entering a number between 8 and 128.");
   console.log(passwordLength);
 
   var characterTypes = [];
 
   var lowerCase = confirm("Would you like to include lowercase characters?");
+  if (lowerCase === true) {
+    characterTypes.push(0);
+  }
   console.log(lowerCase);
+
   var upperCase = confirm("Would you like to include uppercase characters?");
+  if (upperCase === true) {
+    characterTypes.push(1);
+  }
   console.log(upperCase);
+
   var numbers = confirm("Would you like to include numbers?");
+  if (numbers === true) {
+    characterTypes.push(2);
+  }
   console.log(numbers);
+
   var special = confirm("Would you like to include special characters?");
+  if (special === true) {
+    characterTypes.push(3);
+  }
   console.log(special);
+
+  console.log(characterTypes);
 }
 
 // Write password to the #password input
